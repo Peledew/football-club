b844057b05573cef45f72915e23ae5cffdc15cb0
Luka-0 <93083090+Peledew@users.noreply.github.com>
2025-03-01 17:46:11 +0100

    Make migrations for ERD

---
+++ football-club/database/migrations/2025_03_01_152813_create_players_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('last_name');
+            $table->string('ssn')->unique();
+            $table->date('date_of_birth');
+            $table->enum('position', ['CB', 'DMF', 'LB', 'CF']);
+            $table->foreignId('place_id')->constrained();
+            $table->foreignId('club_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('players');
+    }
+};
---
+++ football-club/database/migrations/2025_03_01_152829_create_games_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->date('date_of_event');
+            $table->foreignId('competition_id')->constrained()->onDelete('cascade');
+            $table->foreignId('home_club_id')->constrained('clubs')->onDelete('cascade');
+            $table->foreignId('guest_club_id')->constrained('clubs')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
