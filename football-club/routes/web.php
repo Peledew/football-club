8d4218f0691813a6e16784726930ce7e168a227f
Luka-0 <93083090+Peledew@users.noreply.github.com>
2025-03-05 02:36:11 +0100

    minor change

--- football-club/routes/web.php
+++ football-club/routes/web.php
@@ -6,9 +6,9 @@ use App\Http\Controllers\GameController;
 use App\Http\Controllers\PlaceController;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/places/create', function () {
     return view('places.create');
